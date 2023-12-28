03a61ddb6a6ec7977b46fab1a8c4422b96a0576e
Rumaisha-Bashar45 <76790255+Rumaisha-Bashar45@users.noreply.github.com>
2023-12-29 01:18:58 +0600

    init

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\ARGHYA\OneDrive\Desktop\proj\e-com\venv
+set VIRTUAL_ENV=C:\Users\USER\Desktop\E-Commerce\E-commerce-arghya-rumaisha\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
